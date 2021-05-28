2fb88877ee9a3bcdb7ae6fdd67d3eef51f79425e
Lorenzo <lorenzo@Lorenzos-MacBook-Pro.local>
2021-05-28 19:23:18 -0300

    fix

--- main.js
+++ main.js
@@ -7,20 +7,18 @@ async function run(){
 
     const { context } = github;
 
-    let lastBuildYear = 2021;
-    let lastBuildMonth = 5;
-    let lastBuildDay = 1;
-    let lastBuildHour = 0;
-    let lastBuildMinutes = 0;
-    let lastBuildSeconds = 0;
-
-    let lastBuildDate = new Date(lastBuildYear, lastBuildMonth, lastBuildDay, lastBuildHour, lastBuildMinutes, lastBuildSeconds);
+    let lastBuildYear = '2021';
+    let lastBuildMonth = '05';
+    let lastBuildDay = '01';
+    let lastBuildHour = '00';
+    let lastBuildMinutes = '00';
+    let lastBuildSeconds = '00';
 
     let requestObject = { 
         owner: context.payload.repository.owner.login,
         repo: context.payload.repository.name,
-        labels: ['PROD'],
-        //since: lastBuildDate.toISOString()
+        //labels: ['PROD'],
+        //since: `${lastBuildYear}-${lastBuildMonth}-${lastBuildDay}T${lastBuildHour}:${lastBuildMinutes}:${lastBuildSeconds}Z`
     }
 
     let issuesSinceLastBuild = await octokit.rest.issues.listForRepo(requestObject);
