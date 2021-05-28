d1f25db20400788bf138b14375a4acd61128db57
Lorenzo <lorenzo@Lorenzos-MacBook-Pro.local>
2021-05-28 18:58:58 -0300

    ADD: base code for the action

---
+++ main.js
@@ -0,0 +1,26 @@
+let core = require("@actions/core");
+let github = require("@actions/github");
+
+let pullRequest = github.context.payload.pull_request;
+
+if (!pullRequest) {
+    throw new Error("No pull request information found");
+}
+
+let baseBranch = pullRequest.base.ref;
+
+let repoToken = core.getInput("repo-token", { required: true });
+let octokit = github.getOctokit(repoToken);
+
+const { context } = github;
+
+switch(baseBranch){
+    case "master": {
+        octokit.rest.issues.addLabels({ issue_number: pullRequest.number, owner: context.payload.repository.owner.login, repo: context.payload.repository.name, labels: ['PROD'] });
+        break;
+    }
+    case "staging": {
+        octokit.rest.issues.addLabels({ issue_number: pullRequest.number, owner: context.payload.repository.owner.login, repo: context.payload.repository.name, labels: ['STG'] });
+        break;
+    }
+}
