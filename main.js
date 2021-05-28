14108f36b2210d9bf8bad1c6798f3d52c5091edf
Lorenzo <lorenzo@Lorenzos-MacBook-Pro.local>
2021-05-28 19:13:51 -0300

    FIX: only show data returned

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ async function run(){
 
     let issuesSinceLastBuild = await octokit.rest.issues.listForRepo(requestObject);
 
-    console.log(issuesSinceLastBuild);
+    console.log(issuesSinceLastBuild.data);
 }
 
 run();
